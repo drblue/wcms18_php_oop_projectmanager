4ce91c47e4879175e663e8627f2be533268914f1
Johan Nordström <johan@digitalvillage.se>
2019-03-14 14:57:54 +0100

    Added mass assignment attributes to Todo model.

--- app/Todo.php
+++ app/Todo.php
@@ -7,6 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Todo extends Model
 {
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array
+	 */
+	protected $fillable = [
+		'title', 'project_id', 'completed',
+	];
+
 	public function project() {
 		return $this->belongsTo(Project::class);
 	}
